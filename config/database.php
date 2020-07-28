10c46460a1d92390fd3268c1d12903a343bc760a
yuanke1128 <936519785@qq.com>
2020-07-28 13:54:25 +0800

    完整上线，无bug

--- config/database.php
+++ config/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'       => '127.0.0.1',
     // 数据库名
-    'database'       => 'bolong_mplus',
+    'database'       => 'tp_admin',
     // 用户名
-    'username'       => 'root',
+    'username'       => 'xxx',
     // 密码
-    'password'       => 'root1128',
+    'password'       => 'xxx',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
